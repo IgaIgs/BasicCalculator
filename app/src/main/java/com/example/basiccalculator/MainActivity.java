9376c7c8898a164b7dfb22e4982e6cd471bf1d1e
IgaIgs <szczuczka8@gmail.com>
2020-11-14 19:56:38 +0100

    Expanded the functionality to enable the user to perform multiple operations at once with different operators (however, the order of calculation is not maintained, this is still a very simple calculator, mind you) or to repeat the same operation by pressing the "=" button multiple times. The numbers on screen are now also distinguished between whole numbers and floats so that the decimal ending is hidden with whole numbers. Also changed all the string concatenations to use the strings.xml resources to keep everything safe. Therefore, I also changed the switch statement to if/else statements to avoid crashes when the gradle 5 comes out. Updated access modifiers.

--- app/src/main/java/com/example/basiccalculator/MainActivity.java
+++ app/src/main/java/com/example/basiccalculator/MainActivity.java
@@ -15,10 +15,11 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
     // doubles needed for the calculations and current mock getResult method
-    double firstNo, secondNo;
+    private double firstNo, secondNo;
     // string to contain the current operator
-    String op;
-    boolean result;
+    private String op;
+    private boolean result;
+    private boolean multiop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,54 +45,83 @@ public class MainActivity extends AppCompatActivity {
         String com = (String) button.getText();
 
         // set the text on the screen depending on the user's action
-        switch (v.getId()) {
-            case R.id.bCE:
-                Calc.setText("");
-                Num.setText(""); // delete everything on both screens when CE pressed
-                break;
-            case R.id.bplusminus: // show the negative value of chosen number on lower screen
-                if ((l2 != 0) && (Num.getText().charAt(0) != '-')) {
-                    Num.setText("-" + Num.getText());
-                }
-                break;
-            case R.id.bbckspce:
-                if (l2 == 0){  // if nothing on screen - break to avoid out of bounds exception
-                    break;
-                }
-                Num.setText(((String) Num.getText()).substring(0,(l2-1))); // delete last character of the string on the lower screen
-                Calc.setText(((String) Calc.getText()).substring(0,(l1-1)));
-                break;
-            case R.id.bplus: case R.id.bminus: case R.id.bdvde: case R.id.bpercent: case R.id.bmltply:
-                // take the first operand from what's currently on the lower screen
+        int id = v.getId();
+        if (id == R.id.bCE) {
+            Calc.setText("");
+            Num.setText(""); // delete everything on both screens when CE pressed
+        } else if (id == R.id.bplusminus) { // show the negative value of chosen number on lower screen
+            if ((l2 != 0) && (Num.getText().charAt(0) != '-')) {
+                // Num.setText("-" + Num.getText());
+                Num.setText(getString(R.string.twostrings, "-", Num.getText()));
+            }
+        } else if (id == R.id.bbckspce) {
+            if (l2 == 0) {  // if nothing on screen - break to avoid out of bounds exception
+                return;
+            }
+            Num.setText(((String) Num.getText()).substring(0, (l2 - 1))); // delete last character of the string on the lower screen
+            Calc.setText(((String) Calc.getText()).substring(0, (l1 - 1)));
+        } else if (id == R.id.bplus || id == R.id.bminus || id == R.id.bdvde || id == R.id.bpercent || id == R.id.bmltply) {// take the first operand from what's currently on the lower screen
+            if (multiop){
+                secondNo = Double.parseDouble((String) Num.getText());
+                firstNo = CalculatorClass.calculations(firstNo, secondNo, op);
+                // set the operator to currently chosen one
+                op = com;
+                // continue calculations on the upper screen
+                Calc.setText(getString(R.string.threestrings, Calc.getText(), Num.getText(), com));
+            }
+            else{
                 firstNo = Double.parseDouble((String) Num.getText());
                 // set the operator to currently chosen one
                 op = com;
                 // continue showing everything on the upper screen // show what's currently on lower screen on the upper
-                Calc.setText(Num.getText() + com);
-
-                /*// update the lower screen to show the button pressed
-                Num.setText(com);*/
-                break;
-            case R.id.bequal:
+                Calc.setText(getString(R.string.twostrings, Num.getText(), com));
+            }
+            // update the lower screen to show the button pressed
+            Num.setText(com);
+            multiop = true;
+        } else if (id == R.id.bequal) {
+            if (result){ //if this is the case when the user repeats the same operation by pressing "=" multiple times
+                //show the new/continuing calculation on upper screen
+                if (secondNo % 1 == 0){ // if it's a whole number, display without the decimal ending
+                    Calc.setText(getString(R.string.threestringsninteger, Num.getText(), op, (int) secondNo, com));
+                }
+                else { //display as a double to maintain accuracy
+                    Calc.setText(getString(R.string.threestringsndouble, Num.getText(), op, secondNo, com));
+                }
+                // assign the previous result (which is now on the lower screen) as the firstNo
+                firstNo = Double.parseDouble((String) Num.getText());
+                // don't change the secondNo so it's the same as before, as is the operator.
+            }
+            else{
                 // show the whole calculation on the upper screen
-                Calc.setText((String) Calc.getText() + Num.getText() + com);
+                Calc.setText(getString(R.string.threestrings, Calc.getText(), Num.getText(), com));
                 // take the second operand for the calculation from the lower screen
                 secondNo = Double.parseDouble((String) Num.getText());
-                // access the calculator functionality from the CalculatorClass class
-                double output = CalculatorClass.calculations(firstNo,secondNo,op);
-                Num.setText(""+ output);
-                result = true;
-                break;
-            default:
-                /* String s = ((String) Calc.getText()).substring(0,(l1-1)); // get the last character from the upper screen*/
-                // if the previous character was a '0' or
-                // an operator show only the currently chosen value on the lower screen
-                if (Num.getText().equals("0") || Arrays.asList(ops).contains(Num.getText()) || result){
-                    Num.setText(com);
-                    result = false;}
-                // if the previous character was an operand, continue typing values as usual
-                else { Num.setText(Num.getText() + com);}
-                break;
+            }
+
+            // access the calculator functionality from the CalculatorClass class
+            double output = CalculatorClass.calculations(firstNo, secondNo, op);
+
+            if (output % 1 == 0){ // if it's a whole number, display without the decimal ending
+                Num.setText(getString(R.string.stringninteger, "", (int) output));
+            }
+            else { //display as a double to maintain accuracy
+                Num.setText(getString(R.string.stringndouble, "", output));
+            }
+
+            result = true;
+            multiop = false;
+        } else {/* String s = ((String) Calc.getText()).substring(0,(l1-1)); // get the last character from the upper screen*/
+            // if the previous character was a '0' or "=" or
+            // an operator show only the currently chosen value on the lower screen
+            if (Num.getText().equals("0") || Arrays.asList(ops).contains((String) Num.getText()) || result) {
+                Num.setText(com);
+                result = false;
+            }
+            // if the previous character was an operand, continue typing values as usual
+            else {
+                Num.setText((getString(R.string.twostrings, Num.getText(), com)));
+            }
         }
         }
     }
